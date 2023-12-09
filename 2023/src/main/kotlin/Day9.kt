88e34738ae708488972372be88b4c433d9c272eb
a.tabolin <a.tabolin@redmadrobot.com>
2023-12-09 12:36:46 +0300

    [2023] day 9 - refactoring

--- 2023/src/main/kotlin/Day9.kt
+++ 2023/src/main/kotlin/Day9.kt
@@ -2,35 +2,33 @@ object Day9 {
 
     fun solvePart1(input: Sequence<String>): Int {
         return input
-            .map(::parseInput)
-            .map(::collectDiffs)
+            .map { line -> parseInput(line).collectDiffs() }
             .map { diffs -> diffs.foldRight(0) { diff, acc -> acc + diff.last() } }
             .sum()
     }
 
     fun solvePart2(input: Sequence<String>): Int {
         return input
-            .map(::parseInput)
-            .map(::collectDiffs)
+            .map { line -> parseInput(line).collectDiffs() }
             .map { diffs -> diffs.foldRight(0) { diff, acc -> (diff.first() - acc) } }
             .sum()
     }
 
     private fun parseInput(line: String): List<Int> = line.split(' ').map { it.toInt() }
 
-    private fun collectDiffs(numbers: List<Int>): List<List<Int>> {
-        return mutableListOf(numbers).apply {
-            while (true) {
-                val diff = last()
-                val newDiff = mutableListOf<Int>()
-                var prev = diff.first()
-                for (i in 1 until diff.size) {
-                    newDiff.add(diff[i] - prev)
-                    prev = diff[i]
-                }
-                add(newDiff)
-                if (newDiff.all { it == 0 }) break
+    private fun List<Int>.collectDiffs(): List<List<Int>> {
+        return mutableListOf(this).apply {
+            while (last().any { it != 0 }) {
+                add(last().diffByTwo().toList())
             }
+        }
+    }
+
+    private fun List<Int>.diffByTwo(): Sequence<Int> = sequence {
+        var prev = first()
+        for (i in 1 until size) {
+            yield(get(i) - prev)
+            prev = get(i)
         }
     }
 }

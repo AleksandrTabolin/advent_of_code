04b167dbe8c56e59fd9f09684c0bb4cacce9b9b4
a.tabolin <a.tabolin@redmadrobot.com>
2023-12-09 08:31:45 +0300

    [2023] add templates

---
+++ 2023/src/main/kotlin/DayX.kt
@@ -0,0 +1,10 @@
+object DayX {
+
+    fun solvePart1(input: Sequence<String>): Int {
+        return -1
+    }
+
+    fun solvePart2(input: Sequence<String>): Long {
+       return -1
+    }
+}
---
+++ 2023/src/test/kotlin/DayXTest.kt
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DayXTest {
+
+    private val input = """
+    """.trimIndent()
+
+    @Test
+    fun test1p1() {
+        val input = input.splitToSequence("\n")
+        Assertions.assertEquals(-1, DayX.solvePart1(input))
+    }
+
+    @Test
+    fun test2p1() {
+        val input = FileHelper.readByLines("dayX")
+        Assertions.assertEquals(-1, DayX.solvePart1(input))
+    }
+
+    @Test
+    fun test1p2() {
+        val input = input.splitToSequence("\n")
+        Assertions.assertEquals(-1, DayX.solvePart2(input))
+    }
+
+    @Test
+    fun test2p2() {
+        val input = FileHelper.readByLines("dayX")
+        Assertions.assertEquals(-1, DayX.solvePart2(input))
+    }
+}

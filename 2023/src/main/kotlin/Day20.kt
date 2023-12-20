f4d83d2654a32a9d487a0e1df2b7a0d2688ce976
a.tabolin <a.tabolin@redmadrobot.com>
2023-12-20 22:18:55 +0300

    [2023] day20 part 1

---
+++ 2023/src/main/kotlin/Day20.kt
@@ -0,0 +1,120 @@
+import java.util.*
+
+object Day20 {
+
+    fun solvePart1(input: Sequence<String>): Long {
+        val modules = input.parseInput()
+        val counter = SignalCounter()
+
+        repeat(1000) {
+            modules.startCycle(counter, "broadcaster")
+        }
+
+        return counter.lowCounter * counter.highCounter
+    }
+
+    fun solvePart2(input: Sequence<String>): Long {
+        return -1
+    }
+
+    private fun Map<String, Module>.startCycle(counter: SignalCounter, startModule: String) {
+        val queue = LinkedList<Signal>().apply {
+            add(Signal(from = "Button", to = startModule))
+            counter.count(false)
+        }
+
+        while (queue.isNotEmpty()) {
+            val signal = queue.pop()
+            val module = get(signal.to)
+            module ?: continue
+
+            val signalValue: Boolean? = when (module) {
+                is Module.Broadcast -> signal.value
+                is Module.Conjunction -> {
+                    module.state[signal.from] = signal.value
+                    !module.state.values.all { it }
+                }
+                is Module.FlipFlop -> {
+                    if (!signal.value) {
+                        module.state = !module.state
+                        module.state
+                    } else {
+                        null
+                    }
+                }
+            }
+
+            if (signalValue == null) continue
+
+            module.outputs.forEach { nextModule ->
+                counter.count(signalValue)
+                queue.addLast(Signal(module.name, nextModule, signalValue))
+            }
+        }
+    }
+
+    private fun Sequence<String>.parseInput(): Map<String, Module> {
+        val result = mutableMapOf<String, Module>()
+
+        forEach { line ->
+            when {
+                line.startsWith("broadcaster") -> {
+                    val (name, outputs) = line.split(" -> ")
+                    result[name] = Module.Broadcast(name, outputs = outputs.split(", "))
+                }
+
+                line.startsWith("%") -> {
+                    val (left, outputs) = line.split(" -> ")
+                    val name = left.drop(1)
+                    result[name] = Module.FlipFlop(name = name, outputs = outputs.split(", "))
+                }
+
+                line.startsWith("&") -> {
+                    val (left, outputs) = line.split(" -> ")
+                    val name = left.drop(1)
+                    result[name] = Module.Conjunction(name = name, outputs = outputs.split(", "))
+                }
+            }
+        }
+        result.entries.forEach { (name, module) ->
+            module.outputs.forEach { outputName ->
+                (result[outputName] as? Module.Conjunction)?.state?.put(name, false)
+            }
+        }
+        return result
+    }
+
+    data class Signal(val from: String, val to: String, val value: Boolean = false)
+
+    class SignalCounter {
+        var highCounter = 0L
+        var lowCounter = 0L
+
+        fun count(signal: Boolean) {
+            if (signal) {
+                highCounter += 1
+            } else {
+                lowCounter += 1
+            }
+        }
+
+        override fun toString(): String {
+            return "SignalCounter[high: $highCounter, low: $lowCounter]"
+        }
+    }
+
+    sealed class Module {
+        abstract val name: String
+        abstract val outputs: List<String>
+
+        data class Broadcast(override val name: String, override val outputs: List<String>) : Module()
+        data class FlipFlop(override val name: String, var state: Boolean = false, override val outputs: List<String>) :
+            Module()
+
+        data class Conjunction(
+            override val name: String,
+            val state: MutableMap<String, Boolean> = mutableMapOf(),
+            override val outputs: List<String>
+        ) : Module()
+    }
+}
---
+++ 2023/src/test/kotlin/Day20Test.kt
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day20Test {
+
+    private val input1 = """
+        broadcaster -> a, b, c
+        %a -> b
+        %b -> c
+        %c -> inv
+        &inv -> a
+    """.trimIndent()
+
+    private val input2 = """
+        broadcaster -> a
+        %a -> inv, con
+        &inv -> b
+        %b -> con
+        &con -> output
+    """.trimIndent()
+
+    @Test
+    fun test1p1() {
+        val input = input1.splitToSequence("\n")
+        Assertions.assertEquals(32000000L, Day20.solvePart1(input))
+    }
+
+    @Test
+    fun test2p1() {
+        val input = input2.splitToSequence("\n")
+        Assertions.assertEquals(11687500L, Day20.solvePart1(input))
+    }
+
+    @Test
+    fun test3p1() {
+        val input = FileHelper.readByLines("day20")
+        Assertions.assertEquals(912199500, Day20.solvePart1(input))
+    }
+
+    @Test
+    fun test1p2() {
+        val input = input1.splitToSequence("\n")
+        Assertions.assertEquals(-1, Day20.solvePart2(input))
+    }
+
+    @Test
+    fun test2p2() {
+        val input = FileHelper.readByLines("day20")
+        Assertions.assertEquals(-1, Day20.solvePart2(input))
+    }
+}

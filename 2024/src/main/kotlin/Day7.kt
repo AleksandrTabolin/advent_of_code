f0fd663e31258c8225968abbe775678c573402af
a.tabolin <a.tabolin@redmadrobot.com>
2024-12-07 11:19:34 +0300

    [2024] day 7 variant

--- 2024/src/main/kotlin/Day7.kt
+++ 2024/src/main/kotlin/Day7.kt
@@ -1,32 +1,35 @@
 object Day7 {
 
+    private val OP1 = arrayOf(Op.MULT, Op.PLUS)
+    private val OP2 = arrayOf(Op.MULT, Op.PLUS, Op.CONCAT)
+
     fun solvePart1(input: Sequence<String>): Long {
         return input.parseInput().sumOf { (expected, numbers) ->
-            if (numbers.checkVariants1(1, numbers.first(), expected)) expected else 0
+            expected.takeIf {
+                numbers.checkVariants(expected = expected, ops = OP1)
+            } ?: 0
         }
     }
 
     fun solvePart2(input: Sequence<String>): Long {
         return input.parseInput().sumOf { (expected, numbers) ->
-            if (numbers.checkVariants1(1, numbers.first(), expected)
-                || numbers.checkVariants2(1, numbers.first(), expected)
-            ) expected else 0
+            expected.takeIf {
+                numbers.checkVariants(expected = expected, ops = OP1) ||
+                        numbers.checkVariants(expected = expected, ops = OP2)
+            } ?: 0
         }
     }
 
-    private fun List<Long>.checkVariants1(pos: Int, current: Long, expected: Long): Boolean {
-        if (pos == size && current == expected) return true
-        if (current > expected || pos > lastIndex) return false
-        return checkVariants1(pos + 1, current + get(pos), expected) ||
-                checkVariants1(pos + 1, current * get(pos), expected)
+    private fun List<Long>.checkVariants(pos: Int = 1, current: Long = first(), expected: Long, ops: Array<Op>): Boolean = when {
+        pos == size && current == expected -> true
+        current > expected || pos > lastIndex -> false
+        else -> ops.any { checkVariants(pos + 1, it.block(current, get(pos)), expected, ops) }
     }
 
-    private fun List<Long>.checkVariants2(pos: Int, current: Long, expected: Long): Boolean {
-        if (pos == size && current == expected) return true
-        if (current > expected || pos > lastIndex) return false
-        return checkVariants2(pos + 1, current + get(pos), expected) ||
-                checkVariants2(pos + 1, current * get(pos), expected) ||
-                checkVariants2(pos + 1, "$current${get(pos)}".toLong(), expected)
+    enum class Op(val block: (l: Long, r: Long) -> Long) {
+        PLUS({ l, r -> l + r }),
+        MULT({ l, r -> l * r }),
+        CONCAT({ l, r -> "$l$r".toLong() }),
     }
 
     private fun Sequence<String>.parseInput(): Sequence<Pair<Long, List<Long>>> {

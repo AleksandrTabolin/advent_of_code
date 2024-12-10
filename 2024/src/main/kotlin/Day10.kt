89674e805b3cffc650c2c8a76b46934575e3dcee
a.tabolin <a.tabolin@redmadrobot.com>
2024-12-10 09:04:36 +0300

    [2024] day 10 refactoring

--- 2024/src/main/kotlin/Day10.kt
+++ 2024/src/main/kotlin/Day10.kt
@@ -3,58 +3,48 @@ object Day10 {
     private val directions = arrayOf(1 to 0, 0 to 1, -1 to 0, 0 to -1)
 
     fun solvePart1(input: Sequence<String>): Int {
-        return input.parseInput().run { findTrailheads().sumOf { count(0, it) } }
+        return input.parseInput().run {
+            findTrailheads().sumOf {
+                val visited = mutableSetOf<Pair<Int, Int>>()
+                count(0, it) { point ->
+                    if (point in visited) {
+                        true
+                    } else {
+                        visited.add(point)
+                        false
+                    }
+                }
+            }
+        }
     }
 
     fun solvePart2(input: Sequence<String>): Int {
-        return input.parseInput().run { findTrailheads().sumOf { count2(0, it) } }
+        return input.parseInput().run { findTrailheads().sumOf { count(0, it) { false } } }
     }
 
-    private fun List<List<Int>>.count(num: Int, point: Pair<Int, Int>, visited: MutableSet<Pair<Int, Int>> = mutableSetOf()): Int {
-        if (point in visited) return 0
-        visited.add(point)
-
-        if (num == 9) return 1
-        var result = 0
-        val next = num + 1
-        directions.forEach { diff ->
-            val nextPoint = point.first + diff.first to point.second + diff.second
-            if (isInBound(nextPoint) && get(nextPoint.first)[nextPoint.second] == next) {
-                result += count(next, nextPoint, visited)
-            }
-        }
-        return result
-    }
-
-    private fun List<List<Int>>.count2(num: Int, point: Pair<Int, Int>): Int {
-        if (num == 9) return 1
-        var result = 0
-        val next = num + 1
-        directions.forEach { diff ->
-            val nextPoint = point.first + diff.first to point.second + diff.second
-            if (isInBound(nextPoint) && get(nextPoint.first)[nextPoint.second] == next) {
-                result += count2(next, nextPoint)
-            }
-        }
-        return result
+    private fun List<List<Int>>.count(
+        num: Int, point: Pair<Int, Int>,
+        isVisited: (Pair<Int, Int>) -> Boolean
+    ): Int = when {
+        isVisited.invoke(point) -> 0
+        num == 9 -> 1
+        else -> directions
+            .asSequence()
+            .map { point.first + it.first to point.second + it.second }
+            .filter { isInBound(it) && get(it.first)[it.second] == num + 1 }
+            .sumOf { count(num + 1, it, isVisited) }
     }
 
     private fun Sequence<String>.parseInput(): List<List<Int>> {
-        return map { it.map { if (it == '.') -1 else it.digitToInt() } }.toList()
+        return map { line -> line.map { it.digitToInt() } }.toList()
     }
 
     private fun List<List<Int>>.findTrailheads(): List<Pair<Int, Int>> {
-        val result = mutableListOf<Pair<Int, Int>>()
-
-        for (i in indices) {
-            for (j in get(i).indices) {
-                if (get(i)[j] == 0) result.add(i to j)
-            }
+        return foldIndexed(mutableListOf()) { i, acc, row ->
+            row.indices.forEach { j -> if (get(i)[j] == 0) acc.add(i to j) }
+            acc
         }
-
-        return result
     }
 
     private fun List<List<Any>>.isInBound(p: Pair<Int, Int>) = p.first in indices && p.second in get(p.first).indices
-
 }
